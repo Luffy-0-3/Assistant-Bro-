cf0e86fc3e00d4f5f1d238f58ddb245952694c9c
Luffy-0-3 <neerajnagiri@gmail.com>
2025-09-10 11:55:07 +0530

    Create MusicLibrary.py

---
+++ MusicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "wanna be yours": "https://www.youtube.com/results?search_query=wanna+be+yours+slowed+reverb",
+    "perfect": "https://www.youtube.com/watch?v=cNGjD0VG4R8&list=RDcNGjD0VG4R8&start_radio=1",
+    "often": "https://www.youtube.com/watch?v=29aD73pIWAQ&list=RD29aD73pIWAQ&start_radio=1",
+    "fearless": "https://www.youtube.com/watch?v=PeVqgynorvA&list=RDPeVqgynorvA&start_radio=1",
+    "willow tree": "https://www.youtube.com/watch?v=VmUToeI8p8o&list=RDVmUToeI8p8o&start_radio=1"
+}
